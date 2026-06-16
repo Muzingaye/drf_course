11d73ae79ed5466022643540f2d9f482afe4ad8a
Muzingaye <muzingaye.dube@gmail.com>
2026-06-16 14:50:58 +0200

    ecommerce models

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -27,7 +27,9 @@ INSTALLED_APPS = [
       'django_extensions', #Great packaged to access abstract models
     'django_filters', #Used with DRF
     'rest_framework', #DRF package
-    'core', # New app
+    'rest_framework.authtoken',
+    'core', 
+    "ecommerce",
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework_json_api.parsers.JSONParser',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer'
