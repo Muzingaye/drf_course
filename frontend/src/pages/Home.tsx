0a28955ddb1cb7622b4838f0c71e3b3ba826cf64
Muzingaye <muzingaye.dube@gmail.com>
2026-06-17 21:03:59 +0200

    tt

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex py-8">
+      <div className="text-center py-10 px-8 max">
+        <h1 className="text-2xl mb-4 text-[#333]"> Welcome to ShopHub</h1>
+        <p className="text-xl text-[#666] mb-8">Discover more at fair price</p>
+      </div>
+      <div className="max-w-[1200px] mx-auto px-8">
+        <h1 className="">Our products</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
